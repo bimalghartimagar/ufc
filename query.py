23cbfa3ade22aed40732b411b0aaadcd6bafdbb1
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2019-03-01 22:03:05 +0545

    Add select query
    Added select query to filter fighters whose last name
    starts with A or B or C

--- query.py
+++ query.py
@@ -1,3 +1,4 @@
 INSERT_FIGHTER = "INSERT INTO fighters (name) values (%s) ON CONFLICT(name) DO UPDATE SET name=EXCLUDED.name RETURNING id;"
 INSERT_EVENT = "INSERT INTO events (name, date) values (%s,%s)  RETURNING id;"
 INSERT_RELATION = "INSERT INTO join_events_fighters(event_id, fighter_id) values (%s, %s);"
+SELECT_FIGHTERS = "SELECT name FROM fighters WHERE name ~ '(A|B|C)\w+$';"
